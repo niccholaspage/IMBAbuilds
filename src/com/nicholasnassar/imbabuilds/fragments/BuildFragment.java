768d0081cdb9dba0db7a9d3def1bcfe27ef4bc9d
Nicholas Nassar <niccholaspage@gmail.com>
2013-12-22 17:56:52 -0500

    Fix bug with rotation

--- src/com/nicholasnassar/imbabuilds/fragments/BuildFragment.java
+++ src/com/nicholasnassar/imbabuilds/fragments/BuildFragment.java
@@ -4,6 +4,7 @@ import com.nicholasnassar.imbabuilds.R;
 import com.nicholasnassar.imbabuilds.adapter.Item;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -68,9 +69,11 @@ public class BuildFragment extends Fragment implements TitledFragment {
 			
 			settings.setDefaultTextEncodingName("utf-8");
 			
-			webView.loadDataWithBaseURL(null, text, "text/html", "utf-8", null);
-			
-			//webView.loadData(text, "text/html", null);
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+				webView.loadData(text, "text/html", null);
+			} else {
+				webView.loadDataWithBaseURL(null, text, "text/html", "utf-8", null);
+			}
 		}else {
 			webView.restoreState(savedInstanceState);
 		}
